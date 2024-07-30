736e88fa050d7fc806b1a1e30b1b48177d58a857
Duccio <ducciobreschi@gmail.com>
2024-07-30 12:22:17 +0200

    Hide cards without an image

--- subito__hide-annoyances.js
+++ subito__hide-annoyances.js
@@ -2,17 +2,19 @@
 // @name         Subito.it improvements
 // @namespace    http://subito.it/
 // @version      2024-07-30
-// @description  try to take over the world!
-// @author       Kyek
+// @description  Hide annoying elements on Subito.it
+// @author       duckyb
 // @match        https://www.subito.it/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=subito.it
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/duckyb/UserScripts/main/subito__hide-annoyances.js
 // ==/UserScript==
 
 const AD_WRAPPER = '[id^="ad_wrapper_"]';
 const PAGINATION = '[class^="ItemListContainer_container__"]';
 const SNACKBAR = '[class^="styles_snackbar-wrapper__"]';
 const CARD = '[class^="SmallCard-module_card__"]';
+const PLACEHOLDER_IMAGE = '[class^="CardImage-module_placeholder__"]';
 const SOLD_BADGE = '.item-sold-badge';
 
 function hideAdWrappers() {
@@ -33,7 +35,8 @@ function hideSoldItems() {
   if (!cards) return;
   cards.forEach((card) => {
     const soldBadge = card.querySelector(SOLD_BADGE);
-    if (!soldBadge) return;
+    const placeholderImage = card.querySelector(PLACEHOLDER_IMAGE);
+    if (!soldBadge && !placeholderImage) return;
     card.setAttribute('style', 'display: none !important');
   });
 }
